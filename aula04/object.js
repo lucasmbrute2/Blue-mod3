7d3ac0af970ade11e1dce5a5898b19ae44de267c
Lucas <lucasmbrute614@gmail.com>
2021-08-20 14:22:02 -0300

    inserindo classes em JS

---
+++ aula04/object.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express()
+porta = 3001
+app.use(express.json())
+
+carros = []
+
+function Carro(modelo,tipo,combustivel,marca,ano){
+
+    this.modelo = modelo;
+    this.tipo = tipo;
+    this.combustivel = combustivel;
+    this.marca = marca;
+    this.ano = ano;
+}
+
+
+app.get('/',(req,res)=>{
+
+    res.send('Olá')
+})
+
+app.get('/lista',(req,res)=>{
+
+    res.send(carros)
+})
+
+app.post('/addCar',(req,res)=>{
+
+    let carro = Carro(
+        req.body.modelo,
+        req.body.tipo,
+        req.body.combustivel,
+        req.body.marca,
+        req.body.ano
+    )
+    carros.push(carro)
+    res.send("Carro adicionado com sucesso!") 
+})
+
+
+
+
+
+
+app.listen(porta,()=>{
+
+    console.info(`O app está rodando em http://localhost:${porta}.`)
+})
