14b6e52a56cac3c02efcbe53a9a5de8ce7086ebf
Lucas <lucasmbrute614@gmail.com>
2021-09-01 20:07:34 -0300

    changes

--- aula09/index.js
+++ aula09/index.js
@@ -1,39 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const Conn = require('./models/connect/connect')
+const Conn = require("./models/connect/connect");
 
+app.use(express.json());
 
+Conn("localhost", 27017, "musicasRock");
 
-app.use(express.json())
+const Musica = require("./routes/rotas");
+app.use("/musicas", Musica);
 
-Conn("localhost",27017,"musicas");
-
-
-
-const Musica = require('./routes/rotas')
-app.use('/musicas',Musica);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(port,()=>{
-    console.info(`Servidor rodando na porta ${port}`)
-})
+app.listen(port, () => {
+  console.info(`Servidor rodando na porta ${port}`);
+});
--- aula09/models/connect/musicas.js
+++ aula09/models/connect/musicas.js
@@ -1,10 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const musicaModel = new mongoose.Schema({
-    nome: {type:String , required: true},
-    genero: {type:String, required: true}
-})
+  nome: { type: String, required: true },
+  genero: { type: String, required: true },
+  autor: { type: String, required: true },
+  duracao: { type: Number, required: true },
+});
 
-const Musica = mongoose.model('musicas',musicaModel)
+const Musica = mongoose.model("musicasRock", musicaModel);
 
 module.exports = Musica;
